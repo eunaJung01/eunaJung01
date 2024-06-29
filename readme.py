7b3c2924b12516df31be6c22948c4d9fab65a84f
eunaJung01 <christinejung10@daum.net>
2024-06-29 23:37:09 +0900

    feat: fetching latest blog posts

--- readme.py
+++ readme.py
@@ -1,4 +1,11 @@
-## 🐟
+import feedparser
+import time
+
+URL = "http://eunajung01.tistory.com/rss"
+RSS_FEED = feedparser.parse(URL)
+MAX_POST = 3
+
+markdown_text = """## 🐟
 ![header](https://capsule-render.vercel.app/api?type=waving&color=0:FFFFFF,100:674b61&height=160&section=header)
 
 <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FeunaJung01&count_bg=%23674B61&title_bg=%23332A2A&icon=&icon_color=%23E7E7E7&title=hello&edge_flat=false"/></a>
@@ -7,9 +14,19 @@
 건국대학교 컴퓨터공학부 20학번
 
 ### Latest Blog Posts
-[2024-06-09: Optimistic Version Locking](http://eunajung01.tistory.com/160) <br/>
-[2024-05-13: How to Debug in a Multi-threaded Parallel Program](http://eunajung01.tistory.com/159) <br/>
-[2024-05-05: Blocking vs. Non-Blocking / Synchronous vs. Asynchronous](http://eunajung01.tistory.com/158) <br/>
-[2024-01-01: Bulk-Loading of a B+-Tree (Bottom-Up Approach)](http://eunajung01.tistory.com/157) <br/>
+"""
 
+for idx, feed in enumerate(RSS_FEED['entries']):
+    if idx > MAX_POST:
+        break
+    else:
+        feed_date = feed['published_parsed']
+        markdown_text += f"[{time.strftime('%Y-%m-%d', feed_date)}: {feed['title']}]({feed['link']}) <br/>\n"
+
+markdown_text += """
 [![Solved.ac 프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj=christinejung10)](https://solved.ac/christinejung10)
+"""
+
+f = open("README.md", mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
